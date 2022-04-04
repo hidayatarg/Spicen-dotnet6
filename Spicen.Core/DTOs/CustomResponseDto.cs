579adffbcab7fd61f1c273572e601fae57c45dce
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-04-04 16:06:11 +0300

    [Core] Added CustomResponseDto

---
+++ Spicen.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Spicen.Core.DTOs
+{
+    /// <summary>
+    /// return response with a data
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public List<string> Errors { get; set; }
+
+        public static CustomResponseDto<T> Succcess(int statusCode, T Data)
+        {
+            return new CustomResponseDto<T> { Data = Data, StatusCode = statusCode };
+        }
+
+        public static CustomResponseDto<T> Success(int statusCode)
+        {
+            return new CustomResponseDto<T> { StatusCode=statusCode };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = errors };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, string errors)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { errors } };
+        }
+
+
+    }
+}
---
+++ Spicen.Core/DTOs/NoContentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spicen.Core.DTOs
+{
+    /// <summary>
+    /// return response with no data
+    /// </summary>
+    public class NoContentDto
+    {
+    }
+}
