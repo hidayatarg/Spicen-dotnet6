60c64bbe3b8330a923c87c2a8daea3b0e6864467
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-04-05 13:31:35 +0300

    [API] Implemented All Method Product Controller

---
+++ Spicen.API/Controllers/ProductControllers.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Spicen.Core;
+using Spicen.Core.DTOs;
+using Spicen.Core.Services;
+
+namespace Spicen.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductControllers : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        // DI
+        public ProductControllers(IService<Product> service, IMapper mapper)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+
+       public async Task<IActionResult> All()
+       {
+            var products = await _service.GetAllAsync();
+
+            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+            return Ok(CustomResponseDto<List<ProductDto>>.Succcess(200, productsDtos);
+       }
+
+
+        
+
+    }
+}
