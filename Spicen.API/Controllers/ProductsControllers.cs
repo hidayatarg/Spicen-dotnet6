25dea73b97b0906feadcdd252841c1b2383ccfd2
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-04-10 14:23:24 +0300

    [Test] Product Not Found in Controller

--- Spicen.API/Controllers/ProductsControllers.cs
+++ Spicen.API/Controllers/ProductsControllers.cs
@@ -35,6 +35,11 @@ namespace Spicen.API.Controllers
         public async Task<IActionResult> GetById(int id)
         {
             var product = await _service.GetByIdAsync(id);
+            if (product == null)
+            {
+                return CreateActionResult(CustomResponseDto<ProductDto>.Fail(400, $"Product with {id} Not Found"));
+            }
+
             var productDto = _mapper.Map<ProductDto>(product);
             return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productDto));
         }
