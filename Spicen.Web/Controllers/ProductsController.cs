c69f5e4f8380c917447192ff36b2693b88afebe5
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-08-31 00:35:20 +0300

    Product Controler-1

---
+++ Spicen.Web/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spicen.Core;
+using Spicen.Core.Services;
+
+namespace Spicen.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _service;
+
+        // DI
+        public ProductsController(IProductService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _service.GetProductsWithCategory();
+
+            return View(response.Data);
+        }
+    }
+}
